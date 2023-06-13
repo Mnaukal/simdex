c31599550a73aea411e44d106cee9de2f174bd19
Michal Töpfer <michal.topfer@gmail.com>
2023-06-13 15:20:35 +0200

    Fix typo

--- simulation/run_experiments.sh
+++ simulation/run_experiments.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-for SEED in 1, 2, 3, 4, 5
+for SEED in 1 2 3 4 5
 do
   python3 main.py --config ./experiments/user_experience_limits.yaml --refs ../data/release02-2023-01-04/ref-solutions.csv ../data/release02-2023-01-04/data.csv.gz --seed $SEED
   python3 main.py --config ./experiments/user_experience_oracle.yaml --refs ../data/release02-2023-01-04/ref-solutions.csv ../data/release02-2023-01-04/data.csv.gz --seed $SEED
